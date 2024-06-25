19d1503da2b2f37f732055490c9b82a545bca836
shivapabbu1 <shivaprasad.pabbu@gmail.com>
2024-06-25 16:04:33 +0530

    update

--- src/main/java/com/demo/SpringAopApplication.java
+++ src/main/java/com/demo/SpringAopApplication.java
@@ -3,9 +3,11 @@ package com.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableTransactionManagement
 public class SpringAopApplication {
 
 	public static void main(String[] args) {
